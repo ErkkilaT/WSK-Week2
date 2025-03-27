786b223a45d274c48c24f1bd13374c3dfc1f6a80
Teemu Erkkilä <teemu.erkkila@tuni.fi>
2025-03-27 14:44:31 +0200

    task 5 in dom done

--- dom-assignments/t5/t5.js
+++ dom-assignments/t5/t5.js
@@ -771,3 +771,14 @@ const restaurants = [
 ];
 
 // your code here
+var map = L.map('map').setView([62.237401, 25.614197], 6);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+for (let restaurant of restaurants) {
+  var marker = L.marker(restaurant.location.coordinates.reverse()).addTo(map);
+  marker.bindPopup(`<h3>${restaurant.name}</h3><p>${restaurant.address}</p>`);
+}
