05bf18b976853292922ef7b7b98ac911114ff4a4
Teemu Erkkilä <teemu.erkkila@tuni.fi>
2025-03-30 14:54:43 +0300

    adv1 done

--- advanced1-assignments/adv1.js
+++ advanced1-assignments/adv1.js
@@ -1,9 +1,13 @@
 import {fetchData} from './fetchData.js';
-//import {restaurantRow} from './components.js';
+import {restaurantRow} from './components.js';
+import {restaurantModal} from './components.js';
+import {baseUrl} from './baseUrl.js';
+//const baseUrl =
+('https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants');
+
 //get restaurant data
-const restaurants = await fetchData(
-  'https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants'
-);
+
+const restaurants = await fetchData(baseUrl);
 console.log(restaurants);
 
 //sort data
@@ -18,69 +22,18 @@ const table = document.querySelector('table');
 const modal = document.querySelector('#modal');
 let lastHighlight;
 for (const restaurant of restaurants) {
-  const tr = document.createElement('tr');
+  const tr = restaurantRow(restaurant);
   tr.addEventListener('click', async () => {
     lastHighlight?.classList.remove('highlight');
     tr.classList.add('highlight');
     lastHighlight = tr;
 
     modal.innerHTML = '';
-    const nameH3 = document.createElement('h3');
-    nameH3.innerText = restaurant.name;
-    const addressP = document.createElement('p');
-    addressP.innerText = restaurant.address;
-    const postalCodeP = document.createElement('p');
-    postalCodeP.innerText = restaurant.postalCode;
-    const cityP = document.createElement('p');
-    cityP.innerText = restaurant.city;
-    const phoneNumberP = document.createElement('p');
-    phoneNumberP.innerText = restaurant.phone;
-    const companyP = document.createElement('p');
-    companyP.innerText = restaurant.company;
-    const div1 = document.createElement('div');
-    div1.append(nameH3, addressP, postalCodeP, cityP, phoneNumberP, companyP);
-    modal.append(div1);
-
-    const menu = await fetchData(
-      `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/daily/${restaurant._id}/fi`
-    );
-    console.log(menu);
-    if (menu == -2) {
-      //i don't think this should be changed for readability reasons
-      const errorP = document.createElement('p');
-      errorP.innerText = 'Error! Could not retrieve menu!';
-      modal.append(errorP);
-    } else if (menu.courses.length == 0) {
-      const errorP = document.createElement('p');
-      errorP.innerText = 'Error! No available menu for this restaurant';
-      modal.append(errorP);
-    } else {
-      const div2 = document.createElement('div');
-      const dialogTable = document.createElement('table');
-      for (let course of menu.courses) {
-        const dialogTr = document.createElement('tr');
-        const courseName = document.createElement('td');
-        const coursePrice = document.createElement('td');
-        const courseAllergies = document.createElement('td');
-        courseName.innerText = course.name;
-        coursePrice.innerText = course.price;
-        courseAllergies.innerText = course.diets;
-
-        dialogTr.append(courseName, coursePrice, courseAllergies);
-        dialogTable.append(dialogTr);
-      }
-      div2.append(dialogTable);
-      modal.append(document.createElement('hr'), div2);
-    }
+    const menu = await fetchData(baseUrl + '/daily/' + restaurant._id + '/fi');
+    const modalInner = restaurantModal(restaurant, menu);
+    modal.append(modalInner);
     modal.showModal();
   });
 
-  const nameTd = document.createElement('td');
-  nameTd.innerText = restaurant.name;
-  const addressTd = document.createElement('td');
-  addressTd.innerText = restaurant.address;
-  const cityTd = document.createElement('td');
-  cityTd.innerText = restaurant.city;
-  tr.append(nameTd, addressTd, cityTd);
   table.append(tr);
 }
--- advanced1-assignments/components.js
+++ advanced1-assignments/components.js
@@ -1,61 +1,6 @@
 const restaurantRow = (restaurant) => {
+  const {name, address, city} = restaurant;
   const tr = document.createElement('tr');
-  tr.addEventListener('click', async () => {
-    lastHighlight?.classList.remove('highlight');
-    tr.classList.add('highlight');
-    lastHighlight = tr;
-
-    modal.innerHTML = '';
-    const nameH3 = document.createElement('h3');
-    nameH3.innerText = restaurant.name;
-    const addressP = document.createElement('p');
-    addressP.innerText = restaurant.address;
-    const postalCodeP = document.createElement('p');
-    postalCodeP.innerText = restaurant.postalCode;
-    const cityP = document.createElement('p');
-    cityP.innerText = restaurant.city;
-    const phoneNumberP = document.createElement('p');
-    phoneNumberP.innerText = restaurant.phone;
-    const companyP = document.createElement('p');
-    companyP.innerText = restaurant.company;
-    const div1 = document.createElement('div');
-    div1.append(nameH3, addressP, postalCodeP, cityP, phoneNumberP, companyP);
-    modal.append(div1);
-
-    const menu = await fetchData(
-      `https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants/daily/${restaurant._id}/fi`
-    );
-    console.log(menu);
-    if (menu == -2) {
-      //i don't think this should be changed for readability reasons
-      const errorP = document.createElement('p');
-      errorP.innerText = 'Error! Could not retrieve menu!';
-      modal.append(errorP);
-    } else if (menu.courses.length == 0) {
-      const errorP = document.createElement('p');
-      errorP.innerText = 'Error! No available menu for this restaurant';
-      modal.append(errorP);
-    } else {
-      const div2 = document.createElement('div');
-      const dialogTable = document.createElement('table');
-      for (let course of menu.courses) {
-        const dialogTr = document.createElement('tr');
-        const courseName = document.createElement('td');
-        const coursePrice = document.createElement('td');
-        const courseAllergies = document.createElement('td');
-        courseName.innerText = course.name;
-        coursePrice.innerText = course.price;
-        courseAllergies.innerText = course.diets;
-
-        dialogTr.append(courseName, coursePrice, courseAllergies);
-        dialogTable.append(dialogTr);
-      }
-      div2.append(dialogTable);
-      modal.append(document.createElement('hr'), div2);
-    }
-    modal.showModal();
-  });
-
   const nameTd = document.createElement('td');
   nameTd.innerText = restaurant.name;
   const addressTd = document.createElement('td');
@@ -63,6 +8,56 @@ const restaurantRow = (restaurant) => {
   const cityTd = document.createElement('td');
   cityTd.innerText = restaurant.city;
   tr.append(nameTd, addressTd, cityTd);
+  return tr;
 };
 
-//export restaurantRow;
+const restaurantModal = (restaurant, menu) => {
+  //restaurant info
+  const nameH3 = document.createElement('h3');
+  nameH3.innerText = restaurant.name;
+  const addressP = document.createElement('p');
+  addressP.innerText = restaurant.address;
+  const postalCodeP = document.createElement('p');
+  postalCodeP.innerText = restaurant.postalCode;
+  const cityP = document.createElement('p');
+  cityP.innerText = restaurant.city;
+  const phoneNumberP = document.createElement('p');
+  phoneNumberP.innerText = restaurant.phone;
+  const companyP = document.createElement('p');
+  companyP.innerText = restaurant.company;
+  const div1 = document.createElement('div');
+  div1.append(nameH3, addressP, postalCodeP, cityP, phoneNumberP, companyP);
+
+  //menu info
+  const div2 = document.createElement('div');
+  const dialogTable = document.createElement('table');
+  if (menu == -2) {
+    const errorP = document.createElement('p');
+    errorP.innerText = 'Error! Could not retrieve menu!';
+    div2.append(errorP);
+  } else if (menu.courses.length == 0) {
+    const errorP = document.createElement('p');
+    errorP.innerText = 'Error! No available menu for this restaurant';
+    div2.append(errorP);
+  } else {
+    for (let course of menu.courses) {
+      const dialogTr = document.createElement('tr');
+      const courseName = document.createElement('td');
+      const coursePrice = document.createElement('td');
+      const courseAllergies = document.createElement('td');
+      courseName.innerText = course.name;
+      coursePrice.innerText = course.price;
+      courseAllergies.innerText = course.diets;
+
+      dialogTr.append(courseName, coursePrice, courseAllergies);
+      dialogTable.append(dialogTr);
+    }
+    div2.append(dialogTable);
+  }
+
+  const divFull = document.createElement('div');
+  divFull.append(div1, document.createElement('hr'), div2);
+  return divFull;
+};
+
+export {restaurantRow, restaurantModal};
